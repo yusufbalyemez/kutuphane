6569970d8af1d705af846a04f7c1cecd0df53af4
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2025-02-15 10:13:48 +0300

    kitapları getiren controller (api) oluşturuldu

---
+++ backend/controllers/bookController.js
@@ -0,0 +1,11 @@
+const Book = require('../models/Book');
+
+exports.getAllBooks = async (req,res) => {
+    try {
+        const book = await Book.find({});
+        res.json(book);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message:"Kitaplar getirilirken hata oluştu."});
+    }
+}
