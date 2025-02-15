0c4bf1f0432d111078d5b8b18d5f0ee85cf2362b
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2025-02-15 15:11:28 +0300

    Yazar adına göre kitap getirme işlemi yapıldı.

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,74 +1,99 @@
-const Book = require('../models/bookModel');
+const Book = require("../models/bookModel");
 
-exports.getAllBooks = async (req,res) => {
-    try {
-        const book = await Book.find({});
-        res.json(book);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message:"Kitaplar getirilirken hata oluştu."});
+exports.getAllBooks = async (req, res) => {
+  try {
+    const book = await Book.find({});
+    res.json(book);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Kitaplar getirilirken hata oluştu." });
+  }
+};
+
+exports.getBookById = async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    const book = await Book.findById(bookId);
+    if (!book) {
+      return res.status(404).json({ message: "Kitap bulunamadı." });
     }
-}
+    res.json(book);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Kitap getirilirken hata oluştu." });
+  }
+};
 
-exports.getBookById = async (req,res)=> {
-    try {
-        const bookId = req.params.id;
-        const book = await Book.findById(bookId);
-        if(!book){
-            return res.status(404).json({message:"Kitap bulunamadı."});
-        }
-        res.json(book);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message:"Kitap getirilirken hata oluştu."});
+exports.getBookWithAuthor = async (req, res) => {
+  try {
+    const author = req.params.author;
+    const books = await Book.find({ author: author });
+    if (books.length === 0) {
+      return res.status(404).json({ message: "Yazarın kitapları bulunamadı." });
     }
-}
+    res.json(books);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Yazarın kitapları getirilirken hata oluştu." });
+  }
+};
 
-exports.addBook = async (req,res) => {
-    try {
-        const {title,author,numberOfPages} = req.body;
-        const newBook = new Book({
-            title,
-            author,
-            numberOfPages
-        })
-        await newBook.save();
-        res.status(201).json({message: "Kitap başarıyla eklendi.", book:newBook});
-        //201 oluşturmada 200 ise güncelleme, listeleme, silme sonuçlarında kullanılır.
+exports.addBook = async (req, res) => {
+  try {
+    const { title, author, numberOfPages } = req.body;
+    const newBook = new Book({
+      title,
+      author,
+      numberOfPages,
+    });
+    await newBook.save();
+    res
+      .status(201)
+      .json({ message: "Kitap başarıyla eklendi.", book: newBook });
+    //201 oluşturmada 200 ise güncelleme, listeleme, silme sonuçlarında kullanılır.
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Kitap ekleme sırasında bir hata oluştu." });
+  }
+};
 
-        
-    }catch (error) {
-        console.error(error);
-        res.status(500).json({message: "Kitap ekleme sırasında bir hata oluştu."});
+exports.deleteBook = async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    const deletedBook = await Book.findOneAndDelete({ _id: bookId });
+    if (!deletedBook) {
+      return res
+        .status(404)
+        .json({ message: "Silmek istediğiniz kitap bulunamadı" });
     }
-}
+    res.json({ message: "Kitap başarıyla silindi." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Kitap silme sırasında bir hata oluştu." });
+  }
+};
 
-exports.deleteBook = async (req,res) => {
-    try {
-        const bookId = req.params.id;
-        const deletedBook = await Book.findOneAndDelete({_id:bookId});
-        if(!deletedBook){
-            return res.status(404).json({message:"Silmek istediğiniz kitap bulunamadı"});
-        }
-        res.json({message:"Kitap başarıyla silindi."});
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message:"Kitap silme sırasında bir hata oluştu."});
-    }
-}
-
-exports.updateBook = async (req,res) => {
-    try {
-        const bookId = req.params.id;
-        const {title,author,numberOfPages} = req.body;
-        const updatedBook = await Book.findByIdAndUpdate(bookId,
-            {title,author,numberOfPages},
-            {new:true}
-        )
-        res.status(200).json({message:"Kitap başarıyla güncellendi.",book:updatedBook});
-        //Güncelleme olduğu için 200 status kodu kullanılır.
-    } catch (error){
-        console.error(error);
-        res.status(500).json({message:"Kitap güncelleme sırasında bir hata oluştu."});
-    }
-}
+exports.updateBook = async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    const { title, author, numberOfPages } = req.body;
+    const updatedBook = await Book.findByIdAndUpdate(
+      bookId,
+      { title, author, numberOfPages },
+      { new: true }
+    );
+    res
+      .status(200)
+      .json({ message: "Kitap başarıyla güncellendi.", book: updatedBook });
+    //Güncelleme olduğu için 200 status kodu kullanılır.
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Kitap güncelleme sırasında bir hata oluştu." });
+  }
+};
--- backend/routes/bookRoute.js
+++ backend/routes/bookRoute.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
-const {getAllBooks,addBook,deleteBook,updateBook,getBookById} = require('../controllers/bookController');
+const {getAllBooks,addBook,deleteBook,updateBook,getBookById,getBookWithAuthor} = require('../controllers/bookController');
 
 router.get('/getir',getAllBooks);
 router.get('/getir/:id',getBookById);
+router.get('/getirYazar/:author',getBookWithAuthor);
 router.post('/ekle',addBook);
 router.delete('/sil/:id',deleteBook);
 router.put('/guncelle/:id',updateBook);
+
 
 
 module.exports = router;
