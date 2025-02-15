8b097c6d16a215dbb3555ef90d3d34713e9d31d1
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2025-02-15 10:48:59 +0300

    kitap apisi ve rotaları oluşturuldu

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,4 +1,4 @@
-const Book = require('../models/Book');
+const Book = require('../models/bookModel');
 
 exports.getAllBooks = async (req,res) => {
     try {
--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,14 @@ const express = require('express');
 const connectDB = require('./config/db');
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
+const bookRoute = require('./routes/bookRoute');
 
 
 connectDB();
 
 const app = express();
 
-app.get('/', (req,res)=> {
-    res.json('Node API çalışıyor.')
-});
+app.use("/api/books",bookRoute);
 
 app.listen(PORT, ()=> {
     console.log(`Sunucu ${PORT} portunda çalışıyor.`);
